a8c2eb1aca48d0af478758101e3fc3a099bb98bc
Dmenk123 <rizkiyuandaa@gmail.com>
2018-10-10 16:09:39 +0700

    220-payroll-manajemen commit #2

--- app/Http/Controllers/Hrd/PayrollmanController.php
+++ app/Http/Controllers/Hrd/PayrollmanController.php
@@ -380,9 +380,9 @@ class PayrollmanController extends Controller
                 
             }
             //potongan kehadiran + makan + transport
-            for ($i=0; $i < count($request->harian); $i++) 
+            for ($i=0; $i < count($request->harianmakantrans); $i++) 
             {
-                if ($request->pharianmakantrans[$i] != 0) {
+                if ($request->harianmakantrans[$i] != 0) {
                     $pharianmakantrans = new d_potongan;
                     $pharianmakantrans->d_pot_pid = $request->i_pegawai;
                     $pharianmakantrans->d_pot_prollid = $lastId;
@@ -453,8 +453,10 @@ class PayrollmanController extends Controller
         $payroll = d_payroll_man::join('m_pegawai_man', 'd_payroll_man.d_pm_pid', '=', 'm_pegawai_man.c_id')
                             ->join('m_divisi', 'm_pegawai_man.c_divisi_id', '=', 'm_divisi.c_id')
                             ->join('m_jabatan', 'm_pegawai_man.c_jabatan_id', '=', 'm_jabatan.c_id')
-                            ->select('d_payroll_man.*', 'm_pegawai_man.c_id', 'm_pegawai_man.c_nama' ,'m_pegawai_man.c_divisi_id', 'm_pegawai_man.c_jabatan_id', 'm_divisi.c_divisi', 'm_jabatan.c_posisi', 'm_jabatan.c_sub_divisi_id')
+                            ->select('d_payroll_man.*', 'm_pegawai_man.c_id', 'm_pegawai_man.c_nama' ,'m_pegawai_man.c_divisi_id', 'm_pegawai_man.c_jabatan_id', 'm_pegawai_man.c_tunjangan', 'm_divisi.c_divisi', 'm_jabatan.c_posisi', 'm_jabatan.c_sub_divisi_id')
                             ->where('d_payroll_man.d_pm_id', $id)->first();
+
+        //$list = explode(",", $payroll->c_tunjangan);
 
         $list_tunjangan = d_payroll_mandt::join('d_payroll_man', 'd_payroll_mandt.d_pmdt_pmid', '=', 'd_payroll_man.d_pm_id')
                             ->join('m_tunjangan_man', 'd_payroll_mandt.d_pmdt_typeid', '=', 'm_tunjangan_man.tman_id')
@@ -464,6 +466,7 @@ class PayrollmanController extends Controller
                             ->get();
 
         $list_gaji = d_payroll_mandt::select('d_pmdt_nilai')->where('d_pmdt_type', "GJ")->where('d_pmdt_pmid', $id)->first();
+        $list_potongan = d_potongan::where('d_pot_prollid', $id)->where('d_pot_pid', $payroll->c_id)->get();
 
         //dd($payroll, $list_tunjangan, $list_gaji);
 
@@ -471,7 +474,8 @@ class PayrollmanController extends Controller
             'status' => 'sukses',
             'payroll' => $payroll,
             'list_tunjangan' => $list_tunjangan,
-            'list_gaji' => $list_gaji
+            'list_gaji' => $list_gaji,
+            'list_potongan' => $list_potongan
         ]);
     }
 
