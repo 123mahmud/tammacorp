982d65dd0d08f1e33b873ed31c9dfe1b1aa8c72b
123mahmud <37867983+123mahmud@users.noreply.github.com>
2018-11-14 18:51:34 +0700

    kpi

--- resources/views/hrd/datainputkpi/modal.blade.php
+++ resources/views/hrd/datainputkpi/modal.blade.php
@@ -7,7 +7,7 @@
       <div class="modal-content">
         <div class="modal-header" style="background-color: #e77c38;">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title" style="color: white;">Input Data KPI</h4>
+          <h4 class="modal-title" style="color: white;">Input Data Scoreboard</h4>
         </div>
 
         <div class="modal-body">
