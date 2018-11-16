864639cb2d24badb6e9a383c51d27e2be4efb51f
foxbloodiron <ariakbar6944@gmail.com>
2018-11-16 14:01:51 +0700

    Update _sidebar.blade.php

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -77,10 +77,6 @@
                         <li><a href="{{ url('/logout') }}"><i class="fa fa-sign-out"></i>Log Out</a></li>
                     </ul>
                 </li>
-                <li id="topbar-chat" class="hidden-xs"><a href="javascript:void(0)" data-step="4"
-                                                          data-intro="&lt;b&gt;Form chat&lt;/b&gt; keep you connecting with other coworker"
-                                                          data-position="left" class="btn-chat"><i
-                                class="fa fa-comments"></i><span class="badge badge-info">3</span></a></li>
             </ul>
         </div>
     </nav>
@@ -1184,88 +1180,4 @@
         <!--BEGIN BACK TO TOP-->
         <a id="totop" href="#"><i class="fa fa-angle-up"></i></a>
         <!--END BACK TO TOP-->
-        <!--BEGIN CHAT FORM-->
-        <div id="chat-form" class="fixed">
-            <div class="chat-inner">
-                <h2 class="chat-header">
-                    <a href="javascript:;" class="chat-form-close pull-right"><i class="glyphicon glyphicon-remove">
-                        </i></a><i class="fa fa-users"></i>&nbsp; Message &nbsp;<span class="badge badge-info">3</span>
-                </h2>
-                <div id="group-1" class="chat-group">
-                    <strong>Favorites</strong><a href="#"><span class="user-status is-online"></span>
-                        <small>
-                            Verna Morton
-                        </small>
-                        <span class="badge badge-info">2</span></a><a href="#"><span
-                                class="user-status is-online"></span>
-                        <small>Delores Blake</small>
-                        <span class="badge badge-info is-hidden">
-                                    0</span></a><a href="#"><span class="user-status is-busy"></span>
-                        <small>Nathaniel Morris</small>
-                        <span class="badge badge-info is-hidden">0</span></a><a href="#"><span
-                                class="user-status is-idle"></span>
-                        <small>Boyd Bridges</small>
-                        <span class="badge badge-info is-hidden">0</span></a><a
-                            href="#"><span class="user-status is-offline"></span>
-                        <small>Meredith Houston</small>
-                        <span class="badge badge-info is-hidden">0</span></a></div>
-                <div id="group-2" class="chat-group">
-                    <strong>Office</strong><a href="#"><span class="user-status is-busy"></span>
-                        <small>
-                            Ann Scott
-                        </small>
-                        <span class="badge badge-info is-hidden">0</span></a><a href="#"><span
-                                class="user-status is-offline"></span>
-                        <small>Sherman Stokes</small>
-                        <span class="badge badge-info is-hidden">
-                                    0</span></a><a href="#"><span class="user-status is-offline"></span>
-                        <small>Florence
-                            Pierce
-                        </small>
-                        <span class="badge badge-info">1</span></a></div>
-                <div id="group-3" class="chat-group">
-                    <strong>Friends</strong><a href="#"><span class="user-status is-online"></span>
-                        <small>
-                            Willard Mckenzie
-                        </small>
-                        <span class="badge badge-info is-hidden">0</span></a><a
-                            href="#"><span class="user-status is-busy"></span>
-                        <small>Jenny Frazier</small>
-                        <span class="badge badge-info is-hidden">0</span></a><a href="#"><span
-                                class="user-status is-offline"></span>
-                        <small>Chris Stewart</small>
-                        <span class="badge badge-info is-hidden">0</span></a><a
-                            href="#"><span class="user-status is-offline"></span>
-                        <small>Olivia Green</small>
-                        <span class="badge badge-info is-hidden">0</span></a></div>
-            </div>
-            <div id="chat-box" style="top: 400px">
-                <div class="chat-box-header">
-                    <a href="#" class="chat-box-close pull-right"><i class="glyphicon glyphicon-remove">
-                        </i></a><span class="user-status is-online"></span><span class="display-name">Willard
-                            Mckenzie</span>
-                    <small>Online</small>
-                </div>
-                <div class="chat-content">
-                    <ul class="chat-box-body">
-                        <li>
-                            <p>
-                                <img src="{{asset('assets/images/avatar/128.jpg')}}" class="avt"/><span class="user">John Doe</span><span
-                                        class="time">09:33</span></p>
-                            <p>
-                                Hi Swlabs, we have some comments for you.</p>
-                        </li>
-                        <li class="odd">
-                            <p>
-                                <img src="{{asset('assets/images/avatar/48.jpg')}}" class="avt"/><span class="user">Swlabs</span><span
-                                        class="time">09:33</span></p>
-                            <p>
-                                Hi, we're listening you...</p>
-                        </li>
-                    </ul>
-                </div>
-                <div class="chat-textarea">
-                    <input placeholder="Type your message" class="form-control"/></div>
-            </div>
-        </div>
-        <!--END CHAT FORM-->
+
