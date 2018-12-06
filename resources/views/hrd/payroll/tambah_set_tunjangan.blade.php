162341e19644dd9a3ee68054f4bc1ce0093eee1c
123mahmud <37867983+123mahmud@users.noreply.github.com>
2018-12-06 15:22:28 +0700

    Payroll

--- resources/views/hrd/payroll/tambah_set_tunjangan.blade.php
+++ resources/views/hrd/payroll/tambah_set_tunjangan.blade.php
@@ -98,7 +98,7 @@
                       </div>
                       
                       <div class="col-md-6 col-sm-12 col-xs-12">
-                        <input type="button" value="Batal" class="btn btn-danger btn-block" onclick="batal()">
+                        <a type="button" class="btn btn-danger btn-block" href="{{ url('hrd/payroll/setting-gaji') }}">Batal</a>
                       </div>
                       <div class="col-md-6 col-sm-12 col-xs-12">
                         <input type="submit" value="Simpan" class="btn btn-primary btn-block">
