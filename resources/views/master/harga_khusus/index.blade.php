9a82961ad1b5d281c6a9347a751d1a8897d417eb
123mahmud <37867983+123mahmud@users.noreply.github.com>
2018-12-25 15:58:23 +0700

    fix

--- resources/views/master/harga_khusus/index.blade.php
+++ resources/views/master/harga_khusus/index.blade.php
@@ -52,63 +52,69 @@ tr.details td.details-control {
               <div id="alert-tab" class="tab-pane fade in active">
                 <div class="row" style="margin-top:-20px;">
                   <div class="col-md-12 col-sm-12 col-xs-12">
-
-                    <div class="col-md-2 col-sm-4 col-xs-12">
-                      <label class="tebal">Pilih Group :</label>
-                    </div>
-
-                    <div class="col-md-3 col-sm-8 col-xs-12">
-                      <div class="form-group">
-                        <select class="form-control input-sm select2" id="idGroup" onchange="pilihGroup()" >
-                          @foreach ($group as $grup)
-                            <option value="{{ $grup->pg_id }}">{{ $grup->pg_name }}</option>
-                          @endforeach
-                        </select>
+                    <form id="ItemHarga">
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">Pilih Group :</label>
                       </div>
-                    </div>
 
-                    <div class="col-md-12">
-                      <div class="col-md-12 tamma-bg"  style="margin-top: 5px;margin-bottom: 5px;
-                      margin-bottom: 40px; padding-bottom:20px;padding-top:20px;">
-                        <div class="col-md-9">
-                            <label class="control-label tebal" for="">Masukan Kode / Nama</label>
-                            <div class="input-group input-group-sm" style="width: 100%;">
-                                <input type="text" id="namaitem" name="item" class="form-control">
-                                <input type="hidden" id="i_id" name="i_id" class="form-control">
-                                <input type="hidden" id="i_code" name="i_code" class="form-control">
-                                <input type="hidden" id="i_name" name="i_name" class="form-control">
-                                <input type="hidden" id="m_sname" name="m_sname" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <label class="control-label tebal" name="qty">Qty Real</label>
-                            <div class="input-group input-group-sm" style="width: 100%;">
-                                <input type="number" id="qtyReal" name="qtyReal" class="form-control">
-                            </div>
+                      <div class="col-md-3 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <select class="form-control input-sm select2" name="group" id="idGroup" onchange="pilihGroup()" >
+                            @foreach ($group as $grup)
+                              <option value="{{ $grup->pg_id }}">{{ $grup->pg_name }}</option>
+                            @endforeach
+                          </select>
                         </div>
                       </div>
+
+                      <div class="col-md-12">
+                        <div class="col-md-12 tamma-bg"  style="margin-top: 5px;margin-bottom: 5px;
+                        margin-bottom: 40px; padding-bottom:20px;padding-top:20px;">
+                          <div class="col-md-6">
+                              <label class="control-label tebal" for="">Masukan Kode / Nama</label>
+                              <div class="input-group input-group-sm" style="width: 100%;">
+                                  <input type="text" id="bahan_baku" name="bahan_baku" class="form-control">
+                                  <input type="hidden" id="i_id" name="i_id" class="form-control">        
+                                  <input type="hidden" id="i_name" name="i_name" class="form-control">
+                                  <input type="hidden" id="i_code" name="i_code" class="form-control"> 
+                              </div>
+                          </div>
+                          <div class="col-md-3">
+                              <label class="control-label tebal" name="qty">Harga Khusus</label>
+                              <div class="input-group input-group-sm" style="width: 100%;">
+                                  <input type="number" id="qty" name="price" class="form-control text-right">
+                              </div>
+                          </div>
+                          <div class="col-md-3 col-sm-3 col-xs-12">
+                           <label class="control-label tebal">Satuan</label>
+                              <div class="input-group input-group-sm" style="width: 100%;">
+                                <select class="form-control" id="satuan" name="satuan">
+                                </select>
+                              </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="panel-body">
+                      <div class="table-responsive">
+                        <table class="table tabelan table-hover table-responsive table-bordered" width="100%" cellspacing="0" id="tbl_groupitem">
+                          <thead>
+                            <tr>
+                              {{-- <th class="sorting_disabled"></th> --}}
+                              <th class="wd-15p">Kode - Nama Item</th>
+                              <th class="wd-15p">Harga</th>
+                              <th class="wd-15p">Aksi</th>
+                            </tr>
+                          </thead>
+
+                          <tbody>
+
+                          </tbody>
+
+
+                        </table>
+                      </div>
                     </div>
-                    <div class="panel-body">
-                    <div class="table-responsive">
-                      <table class="table tabelan table-hover table-responsive table-bordered" width="100%" cellspacing="0" id="tbl_groupitem">
-                        <thead>
-                          <tr>
-                            {{-- <th class="sorting_disabled"></th> --}}
-                            <th class="wd-15p">Kode - Nama Item</th>
-                            <th class="wd-15p">Harga</th>
-                            <th class="wd-15p">Aksi</th>
-                          </tr>
-                        </thead>
-
-                        <tbody>
-
-                        </tbody>
-
-
-                      </table>
-                    </div>
-                  </div>
-
+                  </form>
                   </div>
 
                 </div>
@@ -119,7 +125,7 @@ tr.details td.details-control {
                     <div class="panel-body">
                         <div id="note-show">
                             <div class="table-responsive" align="right" style="padding-top: 15px;">
-                              <a href="{{ url('master/divisi/pos/tambahposisi/index') }}">
+                              <a href="{{ url('master/grouphargakhusus/tambahgroup') }}">
                                 <button type="button" class="btn btn-box-tool" title="Tambahkan Data Item">
                                   <i class="fa fa-plus" aria-hidden="true"> &nbsp;
                                   </i>Tambah Data</button>
@@ -199,9 +205,106 @@ tr.details td.details-control {
         });
       }
 
+      $( "#bahan_baku" ).focus(function()
+      {
+        var key = 1;
+        $( "#bahan_baku" ).autocomplete({
+          source: baseUrl+'/master/grouphargakhusus/autocomplete/',
+          minLength: 1,
+          select: function(event, ui) {
+            $('#i_id').val(ui.item.id);
+            $('#i_name').val(ui.item.name);
+            Object.keys(ui.item.satuan).forEach(function(){
+              $('#satuan').append($('<option>', { 
+                value: ui.item.id_satuan[key-1],
+                text : ui.item.satuan[key-1]
+                }));
+              key++;
+            });
+            $('#i_code').val(ui.item.i_code);
+            $("input[name='price']").focus();
+            }
+        });
+        $("#satuan").empty();
+        $("#bahan_baku" ).val('');
+        $("input[name='price']").val('');
+      });
+
+    $('#qty').keypress(function(e)
+    {
+        var charCode;
+        if ((e.which && e.which == 13)) 
+        {
+          charCode = e.which;
+        }
+        else if (window.event) 
+        {
+            e = window.event;
+            charCode = e.keyCode;
+        }
+        if ((e.which && e.which == 13))
+        {
+          var bahan_baku  = $('#bahan_baku').val();
+          var qty         = $('#qty').val();
+          var satuan      = $('#satuan').val();
+          if(bahan_baku == '' || qty == '')
+          {
+            toastr.warning('Item dan Jumlah tidak boleh kosong!!');
+            return false;
+          }
+          else
+          {
+            tambahItemHarga();
+              $('#bahan_baku').val('');
+              $('#satuan').val('');
+              $('#qty').val('');
+              $("input[name='bahan_baku']").focus(); 
+               return false;
+          }
+
+        }
+    });
+
+    function tambahItemHarga(){
+      $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+          });
+        $('.submit-group').attr('disabled', 'disabled');
+        $.ajax({
+            url: baseUrl + "/master/grouphargakhusus/tambahItemHarga",
+            type: 'GET',
+            data: $('#ItemHarga').serialize(),
+            success: function (response) {
+                if (response.status == 'sukses') {
+                    pilihGroup();
+                    $("#satuan").empty();
+                    $("#bahan_baku" ).val('');
+                    $("input[name='price']").val('');
+                    iziToast.success({
+                        timeout: 5000,
+                        position: "topRight",
+                        icon: 'fa fa-chrome',
+                        title: '',
+                        message: 'Data Berhasil di Tambah.'
+                    });
+                } else {
+                    iziToast.error({
+                        position: "topRight",
+                        title: '',
+                        message: 'Data Gagal di Update.'
+                    });
+                    $('.submit-group').removeAttr('disabled', 'disabled');
+                }
+            }
+        })
+    }
+
+      var tbGroup = 0;
       function masterGroup(){
         $('#tb_group').dataTable().fnDestroy();
-        $('#tb_group').DataTable({
+        tbGroup = $('#tb_group').DataTable({
             processing: true,
             // responsive:true,
             serverSide: true,
@@ -246,17 +349,16 @@ tr.details td.details-control {
                 instance.hide({
                   transitionOut: 'fadeOutUp'
                 }, toast);
-                var parent = $(a).parents('tr');
-                var id = $(parent).find('.d_id').text();
-                console.log(id);
+                var idGroup = $('#idGroup').val();
+                console.log(idGroup);
                 $.ajax({
                      type: "get",
-                     url: '{{ route('hapus_cust') }}',
-                     data: {id},
+                     url: baseUrl + '/master/grouphargakhusus/itemharga/hapus/'+a,
+                     data: {idGroup},
                      success: function(response){
                           if (response.status=='sukses') {
                             toastr.info('Data berhasil di hapus.');
-                            tbl_customer.ajax.reload();
+                            pilihGroup();
                           }else{
                             toastr.error('Data gagal di simpan.');
                           }
@@ -284,7 +386,7 @@ tr.details td.details-control {
         console.log(id);
         $.ajax({
              type: "get",
-             url: '{{ route('edit_cust') }}',
+             url : baseUrl + "/master/grouphargakhusus/editgroupharga/"+a,
              data: {id},
              success: function(data){
              },
@@ -297,6 +399,64 @@ tr.details td.details-control {
              async: false
            });
       }
+
+  function ubahStatus(a){
+    $('#status'+a).attr('disabled','disabled');
+    $.ajaxSetup({
+      headers: {
+              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+              }
+      });
+    iziToast.show({
+        timeout: false,
+        color: 'red',
+        title: '',
+        message: 'Yakin ingin merubah status.',
+        position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
+        progressBarColor: 'rgb(0, 255, 184)',
+        buttons: [
+          [
+            '<button>Ok</button>',
+            function (instance, toast) {
+              instance.hide({
+                transitionOut: 'fadeOutUp'
+              }, toast);
+              $.ajax({
+                url : baseUrl + "/master/grouphargakhusus/ubahstatusgrup/"+a,
+                type: 'GET',
+                data: a,
+                success:function(response){
+                if (response.status=='sukses') {
+                    iziToast.success({timeout: 5000,
+                                    position: "topRight",
+                                    icon: 'fa fa-chrome',
+                                    title: '',
+                                    message: 'Status brhasil di ganti.'});
+                    tbGroup.ajax.reload();
+                    $('#status'+a).removeAttr('disabled','disabled');
+                  }else{
+                    iziToast.error({position: "topRight",
+                                  title: '',
+                                  message: 'Status gagal di ubah.'});
+                    $('#status'+a).removeAttr('disabled','disabled');
+                  }
+                }
+
+              });
+            }
+          ],
+          [
+            '<button>Close</button>',
+             function (instance, toast) {
+              $('#status'+a).removeAttr('disabled','disabled');
+              instance.hide({
+                transitionOut: 'fadeOutUp'
+              }, toast);
+            }
+          ]
+        ]
+      })
+    }
 
 
 
